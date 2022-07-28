7c14523f12eeec30a441b62009acfb9e87c21376
Jason ALOYAU <jason.aloyau@outlook.fr>
2022-07-28 19:38:16 +0000

    Service _ Energy _ Electricity _ get_the_list_of_countries_by_renewable_electricity_production

--- main.py
+++ main.py
@@ -15,5 +15,23 @@ def welcome():
 @app.get("/energy/electricity/list_of_countries_by_electricity_consumption")
 def energy_electricity_get_the_list_of_countries_by_electricity_consumption():
     return electricity.get_the_list_of_countries_by_electricity_consumption()
+
+
+# Get the list of countries by electricity production
+@app.get("/energy/electricity/list_of_countries_by_electricity_production")
+def energy_electricity_get_the_list_of_countries_by_electricity_production():
+    return electricity.get_the_list_of_countries_by_electricity_production()
+
+
+# Get the list of countries by electricity production by source
+@app.get("/energy/electricity/list_of_countries_by_electricity_production_by_source")
+def energy_electricity_get_the_list_of_countries_by_electricity_production_by_source():
+    return electricity.get_the_list_of_countries_by_electricity_production_by_source()
+
+
+# Get the list of countries by renewable electricity production
+@app.get("/energy/electricity/list_of_countries_by_renewable_electricity_production")
+def energy_electricity_get_the_list_of_countries_by_renewable_electricity_production():
+    return electricity.get_the_list_of_countries_by_renewable_electricity_production()
 # Electricity
 # Energy
